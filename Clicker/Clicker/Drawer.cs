ff76e1d55743b891f251aaf62eebd5207248d581
yegorf1 <yegorf1@gmail.com>
2013-10-06 11:42:37 +0300

    Clicking on cookies and buing cursors

---
+++ Clicker/Clicker/Drawer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace Clicker
+{
+    public static class Drawer
+    {
+        [DllImport("User32.dll")]
+        public static extern IntPtr GetDC(IntPtr hwnd);
+        [DllImport("User32.dll")]
+        public static extern void ReleaseDC(IntPtr hwnd, IntPtr dc);
+
+        public static Graphics g;
+        public static IntPtr desktopPtr;
+
+        public static void Init()
+        {
+            desktopPtr = GetDC(IntPtr.Zero);
+            g = Graphics.FromHdc(desktopPtr);
+        }
+
+        public static void DrawRectangle(Rectangle rect)
+        {
+            SolidBrush b = new SolidBrush(Color.White);
+            g.FillRectangle(b, rect);
+
+            g.Dispose();
+            ReleaseDC(IntPtr.Zero, desktopPtr);
+        }
+
+        public static void DrawString(string str, PointF p)
+        {
+            Init();
+            SolidBrush b = new SolidBrush(Color.White);
+            Font f = new Font("Arial", 12f);
+            
+            g.DrawString(str, f, b, p);
+
+            g.Dispose();
+            ReleaseDC(IntPtr.Zero, desktopPtr);
+        }
+    }
+}
---
+++ Clicker/Clicker/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace Clicker
+{
+    class Program
+    {
+        public static int ClickPrice = 1,
+                          SecondPrice = 0;
+
+        static void Main(string[] args)
+        {
+            Drawer.Init();
+            for (int i = 0; i < 1000; i++)
+            {
+                Thread.Sleep(10);
+                //Cookie();
+                Console.WriteLine(MouseControl.GetCursorPosition().X + ", " + MouseControl.GetCursorPosition().Y);
+                BuyObject(1);
+            }
+        }  
+
+        static void Cookie()
+        {
+            MouseControl.SetCursorPosition(171, 384);
+
+            MouseControl.MouseEvent(MouseControl.MouseEventFlags.LeftDown);
+            MouseControl.MouseEvent(MouseControl.MouseEventFlags.LeftUp);
+        }
+
+        public static void BuyObject(int id)
+        {
+            MouseControl.SetCursorPosition(1225, 200 + id * 40);
+
+            MouseControl.MouseEvent(MouseControl.MouseEventFlags.LeftDown);
+            MouseControl.MouseEvent(MouseControl.MouseEventFlags.LeftUp);
+        }
+
+    }
+}
